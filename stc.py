ea204eb3226c19c9cddb8814e59f71b692bb72fa
Ophelia <natshinapolo@gmail.com>
2022-09-24 19:18:03 +0200

    improved shtoph

--- stc.py
+++ stc.py
@@ -1,4 +1,5 @@
 # made for a vrchat player usernamed Cachie~
+from time import sleep
 from speech_recognition import Microphone, Recognizer, UnknownValueError
 from pythonosc.udp_client import SimpleUDPClient
 
@@ -10,16 +11,17 @@ mic = Microphone()
 
 while True:
     try:
-        print(f"{__file__} is now running\n\nplease be silent for a bit to improve your experience")
-        with mic as back_ground_noise: engine.adjust_for_ambient_noise(back_ground_noise)
-        print(f"minimum energy threshold set to: {engine.energy_threshold}\n\nyou're good to talk!")
+        print(f"{__file__} is now running\n\nbe silent for a moment to improve your experience")
+        sleep(1)
+        with mic as back_ground_noise: engine.adjust_for_ambient_noise(back_ground_noise, duration=1)
+        print(f"minimum energy threshold set to: {engine.energy_threshold}\n\nyou're good to speak!")
         with mic as voice: speech = engine.listen(voice)
         try:
             text = engine.recognize_google(speech)
             print(f"speech recognized as: {text}. sending it to vrchat...")
             vrchat.send_message("/chatbox/input", [text, True]) # True to send and display, False to send only
         #
-        except UnknownValueError: print("didn't quite get that, could you say it again please...")
+        except UnknownValueError: print("didn't quite get that, say it again please...")
     #
     except KeyboardInterrupt: pass
 #
