3bfd837f3a6d6ab65b2316e914079a45ddbccb9f
Ophelia <natshinapolo@gmail.com>
2022-09-24 20:32:03 +0200

    voice detection init button

--- stc.py
+++ stc.py
@@ -1,6 +1,7 @@
 # made for a vrchat player usernamed Cachie~
 from speech_recognition import Microphone, Recognizer, UnknownValueError
 from pythonosc.udp_client import SimpleUDPClient
+from keyboard import wait
 from time import sleep
 
 
@@ -12,16 +13,24 @@ mic = Microphone()
 print(f"{__file__} is now running\n\nbe silent for a moment to improve your experience")
 sleep(1)
 with mic as back_ground_noise: engine.adjust_for_ambient_noise(back_ground_noise, duration=1)
-print(f"minimum energy threshold set to: {engine.energy_threshold}\n\nyou're good to speak!")
+print(f"minimum energy threshold set to: {engine.energy_threshold}\n\npress \"F\" to speak!\n\n")
 
 
-while True:
-    print("now listening...")
+def speech_to_chatbox():
+    print("listening now...")
     with mic as voice: speech = engine.listen(voice)
     try:
         text = engine.recognize_google(speech)
         print(f"speech recognized as: {text}. sending it to vrchat...")
         vrchat.send_message("/chatbox/input", [text, True]) # True to send and display, False to send only
+    except UnknownValueError:
+        print("didn't quite get that, say it again please...")
+        text = ""
     #
-    except UnknownValueError: print("didn't quite get that, say it again please...")
+#
+
+
+while True:
+    wait("f")
+    speech_to_chatbox()
 #
