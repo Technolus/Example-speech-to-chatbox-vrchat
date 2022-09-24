460c5dafabdadbea40abccca4b5ce14a2a95cf1b
Ophelia <natshinapolo@gmail.com>
2022-09-24 19:53:28 +0200

    imporved it a bit

--- stc.py
+++ stc.py
@@ -9,19 +9,19 @@ engine = Recognizer()
 mic = Microphone()
 
 
+print(f"{__file__} is now running\n\nbe silent for a moment to improve your experience")
+sleep(1)
+with mic as back_ground_noise: engine.adjust_for_ambient_noise(back_ground_noise, duration=1)
+print(f"minimum energy threshold set to: {engine.energy_threshold}\n\nyou're good to speak!")
+
+
 while True:
+    print("now listening...")
+    with mic as voice: speech = engine.listen(voice)
     try:
-        print(f"{__file__} is now running\n\nbe silent for a moment to improve your experience")
-        sleep(1)
-        with mic as back_ground_noise: engine.adjust_for_ambient_noise(back_ground_noise, duration=1)
-        print(f"minimum energy threshold set to: {engine.energy_threshold}\n\nyou're good to speak!")
-        with mic as voice: speech = engine.listen(voice)
-        try:
-            text = engine.recognize_google(speech)
-            print(f"speech recognized as: {text}. sending it to vrchat...")
-            vrchat.send_message("/chatbox/input", [text, True]) # True to send and display, False to send only
-        #
-        except UnknownValueError: print("didn't quite get that, say it again please...")
+        text = engine.recognize_google(speech)
+        print(f"speech recognized as: {text}. sending it to vrchat...")
+        vrchat.send_message("/chatbox/input", [text, True]) # True to send and display, False to send only
     #
-    except KeyboardInterrupt: pass
+    except UnknownValueError: print("didn't quite get that, say it again please...")
 #
